f9d0429005979d2c461288d0cdba18fd77ab41fc
Waldemar Zurowski <waldemar.zurowski@r3.com>
2021-12-07 05:54:19 +0100

    Initial revision

---
+++ terraform/modules/moduleA/providers.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_version = ">=1.0.0"
+  required_providers {
+    null = {
+      version = ">=3.1.0"
+    }
+  }
+}
---
+++ terraform/modules/moduleA/variables.tf
@@ -0,0 +1,4 @@
+variable "name" {
+  description = "The name of created resources"
+  type        = string
+}
